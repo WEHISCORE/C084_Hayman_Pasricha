472a1e1c66076b9bc4df647e36e37a7c545abba5
PeteHaitch <peter.hickey@gmail.com>
2020-02-26 09:43:31 +1100

    Update fastqc.sh and its outputs

--- code/fastqc.sh
+++ code/fastqc.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 # Run FastQC (and MultiQC on the output).
 # Peter Hickey
-# 2019-11-12
+# 2019-11-24
 
 module load fastqc
 
@@ -10,9 +10,10 @@ mkdir -p ${OUTDIR}
 
 fastqc -o ${OUTDIR} \
   --threads 10 \
-  ../extdata/NN179/*fastq.gz
+  ../extdata/NN179/*fastq.gz \
+  ../extdata/NN183/merged/*fastq.gz
 
-multiqc --title NN179 \
+multiqc --title NN179_NN183 \
   --outdir ${OUTDIR} \
   --no-megaqc-upload \
   ${OUTDIR}
